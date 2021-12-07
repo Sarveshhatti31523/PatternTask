076031571a5d69770e1767f1acda98bac2a046fe
S.S.HATTI <S.S.HATTI@Sarvesh>
2021-12-07 19:43:16 +0530

    Task

--- src/example/Example1.java
+++ src/example/Example1.java
@@ -8,35 +8,35 @@ import java.util.Scanner;
 public class Example1 {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		
+
 		File Fileright = new File("C:\\Users\\hp\\abc.txt");
-		PrintWriter out = new PrintWriter(Fileright); 
-        int count=1;	
-        System.out.println("Enter the Height of the tree");
+		PrintWriter out = new PrintWriter(Fileright);
+
+		int count = 1;
+		System.out.println("Enter the Height of the tree");
 		Scanner in = new Scanner(System.in);
-		int a=in.nextInt();
-		
-		for(int i=1;i<=a;i++) {
-		  for(int k=1;k<=Math.pow(2,i-1);k++) {
-            
-			  if(i==k) {
-				  out.print("   ");
-			  }
-			  
-			  if(i>=count) {
-				  out.print("   ");
-			  }
-			 
-			  out.print(" "+count+" ");
-			  count++;
-			  	   
-		}	 
-		  
-		out.println();
-		
+		int a = in.nextInt();
+
+		for (int i = 1; i <= a; i++) {
+			for (int k = 1; k <= Math.pow(2, i - 1); k++) {
+
+				if (i == k) {
+					out.print("   ");
+				}
+
+				if (i >= count) {
+					out.print("   ");
+				}
+
+				out.print(" " + count + " ");
+				count++;
+
+			}
+
+			out.println();
+
 		}
-    out.close();
+		out.close();
 	}
 
 }
-
